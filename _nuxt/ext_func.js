ad4a08b168007c6b84732f95c81b4a70f0d8e982
unknown <ggdzqy@qq.com>
2024-04-11 12:25:42 +0800

    modify ext_func

--- _nuxt/ext_func.js
+++ _nuxt/ext_func.js
@@ -1,3 +1,10 @@
 var btn = document.querySelector("div.area.done div.input-group-button");
 console.log(btn);
-//
+btn.addEventListener('click', function (evt) {
+    let v = document.querySelector("input.input-sm").value;
+    console.log(v);
+    let can = document.querySelector("div.area.done div.svg-wrapper.flex");
+    if (v){
+        can.innerHTML = `<img width="360px" max-height="240px" src="${v}">`;
+    }
+});
