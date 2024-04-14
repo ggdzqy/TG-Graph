1df505f1494329e2795a075c4f3ee3be4644d5fc
yu qi <yuqi@yudeiMac.local>
2024-04-14 23:34:14 +0800

    fix time

--- functions/file/[id].js
+++ functions/file/[id].js
@@ -89,6 +89,16 @@ export async function onRequest(context) {
 
       //get time
       let time = new Date().getTime();
+      var today = new Date();
+      //日期
+      var DD = String(today.getDate()).padStart(2, '0'); // 获取日
+      var MM = String(today.getMonth() + 1).padStart(2, '0'); //获取月份，1 月为 0
+      var yyyy = today.getFullYear(); // 获取年
+      // 时间
+      var hh =  String(today.getHours()).padStart(2, '0');       //获取当前小时数(0-23)
+      var mm = String(today.getMinutes()).padStart(2, '0');     //获取当前分钟数(0-59)
+      var ss = String(today.getSeconds()).padStart(2, '0');     //获取当前秒数(0-59)
+      var time_today = yyyy + '-' + MM + '-' + DD + ' ' + hh + ':' + mm + ':' + ss;
 
       let apikey = env.ModerateContentApiKey;
 
@@ -102,7 +112,7 @@ export async function onRequest(context) {
         } else {
           //add image to kv
           await env.img_url.put(params.id, "", {
-            metadata: { ListType: "None", Label: "None", TimeStamp: time },
+            metadata: { ListType: "None", Label: "None", TimeStamp: time_today },
           });
         }
       } else {
