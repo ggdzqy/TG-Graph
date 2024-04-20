70a2998487b060ed8f2c3c69824ee74acdac414e
unknown <ggdzqy@qq.com>
2024-04-21 00:29:14 +0800

    update

--- _nuxt/ext_func.js
+++ _nuxt/ext_func.js
@@ -4,8 +4,15 @@ function doLoad() {
     document.querySelector("div.area.done div.svg-wrapper.flex").innerHTML = `<img style="width:360px;max-height:360px;object-fit:contain;" src="${v}">` }
     , false);
     document.querySelector("div.area.done div.text-area").style="padding: 10px 10px;"
-    document.querySelector("div.area.done div.text-area").innerHTML = `<div id="kv" class="input-group"><div>Date:<input type="text" id="date" class="input-sm">Tag:<input type="text" id="tag" class="input-sm"></div><div>Label:<input type="text" id="label" class="input-sm"></div><div>Name:<input type="text" readonly="readonly"><button id="btnsave" type="button" >保存</button></div>`;
+    document.querySelector("div.area.done div.text-area").innerHTML = `<div id="kv" class="input-group"><div>Date:<input type="text" id="kvdate">Tag:<input type="text" id="kvtag"></div><div>Label:<input type="text" id="kvlabel"></div><div>Name:<input type="text" id="kvname" readonly="readonly"><button id="kvsave" type="button">保存</button></div>`;
     console.log("set DOM")
+    document.querySelector("#kvname").value = `${v}`;
+    document.querySelector("#kvnsave").addEventListener('click', function (e) {
+        let kvdate = document.querySelector("#kvdate").value;
+        let kvtag = document.querySelector("#kvtag").value;
+        let kvlabel = document.querySelector("#kvlabel").value;
+        alert(`${v}?date=${kvdate}&tag=${kvtag}&label=${kvlabel}`);
+        }
 }
 (function(){
     const can = document.querySelector("div.area.done div.svg-wrapper.flex");
