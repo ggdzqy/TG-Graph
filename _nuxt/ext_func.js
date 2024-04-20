8048aa4a6c0f53893ae1d250f0b16713014d8828
unknown <ggdzqy@qq.com>
2024-04-21 00:13:02 +0800

    update

--- _nuxt/ext_func.js
+++ _nuxt/ext_func.js
@@ -3,10 +3,12 @@ function doLoad() {
     let v = document.querySelector("input.input-sm").value;
     document.querySelector("div.area.done div.svg-wrapper.flex").innerHTML = `<img style="width:360px;max-height:360px;object-fit:contain;" src="${v}">` }
     , false);
-    document.querySelector("div.area.done div.text-area").innerHTML = `<div id="kv" class="input-group">Date:<input type="text" id="date" class="input-sm">Tag:<input type="text" id="tag" class="input-sm">Label:<input type="text" id="label" class="input-sm"></div><button id="btnsave" type="button" >保存</button>`;
 }
 (function(){
     const can = document.querySelector("div.area.done div.svg-wrapper.flex");
     const btn = document.querySelector("div.area.done div.input-group-button");
     setTimeout(function(){doLoad();}, 3000)
+    document.querySelector("div.area.done div.text-area").style="padding: 10px 10px;"
+    document.querySelector("div.area.done div.text-area").innerHTML = `<div id="kv" class="input-group"><div>Date:<input type="text" id="date" class="input-sm">Tag:<input type="text" id="tag" class="input-sm"></div><div>Label:<input type="text" id="label" class="input-sm"></div><div>Name:<input type="text" readonly="readonly"><button id="btnsave" type="button" >保存</button></div>`;
+    console.log("set DOM")
 })();
