696804e59490667cfc36e72a8a2c0a320299476f
unknown <ggdzqy@qq.com>
2024-04-24 12:13:13 +0800

    update

--- _nuxt/ext_func.js
+++ _nuxt/ext_func.js
@@ -1,28 +1,24 @@
 
-/**
- * 创建一个固定位置的按钮容器。
- * @returns {HTMLElement} 返回创建的按钮容器元素。
+/*
+ * 创建一个修改上传参数的容器
  */
-function createButtonContainer() {
-    document.querySelector("container.footer").innerHTML = `<div id="kv" class="input-group" align="right"><div>Date:<input type="text" id="date" class="input-sm"></div><div>Tag:<input type="text" id="tag" class="input-sm"></div><div>Label:<input type="text" id="label" class="input-sm"></div><div>Name:<input type="text" readonly="readonly"></div><div><button id="btnsave" type="button">保存</button></div>`;
-}
 
-
-$(function AddDOM() {
+$(function() {
     document.querySelector("body").innerHTML += `<div id="_topbox" style="display: flex; flex-direction: column; align-items: center; position: fixed; top: 15%; right: 1vh; z-index: 1000; background: white; padding:10px; border: 1px solid #c7cfd7; border-radius:5px;">
-    <div id="_infobox">WAIT ...</div>
-    <div class="kv" id="_opbox" align="left"><button id="op">刷新</button></div>
+    <div id="_infobox">WAIT</div>
     </div>`;
+})
 
+$.createUploadInfoContainer = function(){
     //setTimeout(function(){doLoad();}, 500)
-    document.querySelector("#op").addEventListener('click', function() {
+    //document.querySelector("#op").addEventListener('click', function() {
         document.querySelector("#_infobox").innerHTML = `<div class="kv" id="kv" align="right">
         <div>Name:<input class="readonly" id="kvname" type="text" readonly="readonly"></div>
         <div>Org:<input type="text" id="kvorg"></div>
         <div>Date:<input type="text" id="kvdate"></div>
         <div>Tag:<input type="text" id="kvtag"></div>
         <div>Label:<input type="text" id="kvlabel"></div>
-        <div><button id="kvclearcookie" type="button">清除</button>
+        <div><button id="kvclearcookie" type="button">清除Cookie</button>
         <button id="kvsave" type="button">保存</button>
         </div>`;
         document.querySelector("#kvname").value = document.querySelector("input.input-sm").value.split("/file/")[1];
@@ -57,7 +53,6 @@ $(function AddDOM() {
             $.cookie('kvtag', null);
             $.cookie('kvlabel', null);
         }); 
-        console.log("refreshed.");
-    });
-    console.log("Set DOM start.")
-})();
+    //});
+    console.log("createUploadInfoContainer start.")
+}
