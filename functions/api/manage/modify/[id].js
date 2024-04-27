7c6db45c021239d825173af5d1ec5433266e695a
q7nob <q7nobody@gmail.com>
2024-04-27 21:05:36 +0800

    update

--- functions/api/manage/modify/[id].js
+++ functions/api/manage/modify/[id].js
@@ -1,4 +1,4 @@
-export async function onRequest(context) {
+export async function onRequestPost(context) {
     // Contents of context object
     const {
       request, // same as existing Worker API
