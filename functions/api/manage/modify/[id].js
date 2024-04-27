0812742e0fd2f261f4104e33e1e85ab473787220
q7nob <q7nobody@gmail.com>
2024-04-28 01:12:20 +0800

    update

--- functions/api/manage/modify/[id].js
+++ functions/api/manage/modify/[id].js
@@ -9,27 +9,17 @@ export async function onRequestPost(context) {
       data, // arbitrary space for passing data between middlewares
     } = context;
     context.request;
-    //console.log(env)
-    //console.log(params.id)
-    //read the metadata
-    //const value = await env.img_url.getWithMetadata(params.id);
-    var ret={};
 
-    //const vdata = params.id.split("?")[1];
-    //const vjson = JSON.parse(data);
-    for (let [key, value] of data) { 
-      console.log(`${key} : ${value}`);  
-      ret[key] = value;
-    }
-
+    //{"ListType":"public","Label":"None3","TimeStamp":1712817387960,"Tag":"None2","Org":"1"}
+    const url = new URL(request.url);
+    const value = await env.img_url.getWithMetadata(params.id);
     //"metadata":{"TimeStamp":19876541,"ListType":"None","rating_label":"None"}
     //change the metadata
-
-    //await env.img_url.put(params.id,"",{metadata: metadata});
-    //value.metadata.ListType = "public"
-    //value.metadata.Tag = "None"
-    //await env.img_url.put(params.id,"",{metadata: value.metadata});
-    const info = JSON.stringify(ret);
+    value.metadata.Tag = url.searchParams.get("Tag")
+    value.metadata.Label = url.searchParams.get("Label")
+    
+    await env.img_url.put(params.id,"",{metadata: value.metadata});
+    const info = JSON.stringify(value.metadata);
     return new Response(info);
   }
 
